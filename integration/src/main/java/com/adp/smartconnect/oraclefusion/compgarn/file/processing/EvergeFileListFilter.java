1685ac83ca8bba67f516ad460dd633bd41da626c
evergerepo <singha@evergegroup.com>
2017-07-12 21:21:11 +0530

    Modified Kondala's changes

--- integration/src/main/java/com/adp/smartconnect/oraclefusion/compgarn/file/processing/EvergeFileListFilter.java
+++ integration/src/main/java/com/adp/smartconnect/oraclefusion/compgarn/file/processing/EvergeFileListFilter.java
@@ -1,98 +1,96 @@
 package com.adp.smartconnect.oraclefusion.compgarn.file.processing;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 
-import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.integration.file.filters.AbstractFileListFilter;
 
 import com.adp.smartconnect.oraclefusion.compgarn.config.Configuration;
 
 public class EvergeFileListFilter<F> extends AbstractFileListFilter<F> {
 
-	protected static final Logger log = LoggerFactory.getLogger(EvergeFileListFilter.class);
+	private static final Logger log = LoggerFactory.getLogger(EvergeFileListFilter.class);
 	
-	protected Configuration configuration;
+	private Configuration configuration;
 	
 	@Override
 	protected boolean accept(F file) {
-		
+
 		File f = (File) file;
-		
-		if(f.getAbsolutePath().contains(".working") || f.getAbsolutePath().contains(".writing")) {
-			// wait for some time
+
+		MDC.put("fileName", f.getName());
+		log.info("Input File : ["+f.getAbsolutePath()+"]");
+
+		if(f.getAbsolutePath().contains(".working")) {
+			log.warn("File is Working file. Wait for some time to complete process......");
 			return false;
 		}
 		
-		return true;
-		
-	}
-	
-	protected void removeFile(File input) {
-		input.delete();
-	}
-	
-	protected void moveToErrorDir(String errorDir, File file) {
-		
-		File newFile = new File(errorDir + file.getName());
-		try {
-			Files.copy(file.toPath(), newFile.toPath());
-			removeFile(file);
-		} catch (IOException e) {
-			log.error("Error while moving the file to error directory",e);
-		}
-
-	}
-	
-	protected boolean isFileExisting(String archiveLocation, String fileName) {	
-		
-		String fullName = archiveLocation + fileName;
-		log.info("Check if the file has already been processed " + fullName);
-
-		File fl = new File(fullName);
-		if(fl.exists()) {
+		/*
+		 * Check for PQQ file. If file name exists in "archive" folder then remove file from "inbound" folder 
+		 *  and stop processing the file
+		 */
+		if(f.getAbsolutePath().contains(".pqq")) {
+			String pqqArchiveLocation = configuration.getFileProcessingDir() + configuration.getPqqDir() + configuration.getPqqArchiveDir();
+			String archiveFileName = pqqArchiveLocation + f.getName();
+			log.info("Checking if the file has already been processed in archive folder: "+f.getName());
 			
-			log.error("The file has already been processed.");
-			removeFile(fl);
-			return true;
+			File archiveFile = new File(archiveFileName);
+			final File dir = new File(pqqArchiveLocation);
 			
-		}
-		log.info("File " + fl.getAbsolutePath() + " is accepted for processing");
-		return false;
-		
-	}
-	
-	protected String[] getFileExtensions() {
-		return null;
-	}
-	
-	protected boolean shouldFilebeAccepted(File file) {
-		
-		int extensionIndex =FilenameUtils.indexOfExtension(file.getName());
-		String extension = file.getName().substring(extensionIndex);
-		String[] eligibleExtensions = getFileExtensions();
-		boolean shouldFilebeAccepted = false;
-		for(String s : eligibleExtensions) {
-			if(extension.equals(s)) {
-				shouldFilebeAccepted = true;
-				break;
+			File[] files = dir.listFiles();
+			if(null == files) {
+				return true;
 			}
+			
+			if(archiveFile.exists()) {
+				log.error("PQQ file has already been processed, removing file from inbound folder. File Name:"+f.getName());
+				removeFile(f);
+				return false;
+			}else{
+				log.info("PQQ File received " + f.getName());
+			}
+			return true;
+		}else if ((f.getAbsolutePath().contains(".txt") || f.getAbsolutePath().contains(".grn")) && !f.getAbsolutePath().contains(".writing")) {
+			log.info("File " + f.getAbsolutePath() + " is accepted for evaluation");
+			String notifArchiveLocation = configuration.getFileProcessingDir() + configuration.getLienArchiveDir();
+			String fullName = notifArchiveLocation + f.getName();
+			log.info("Check if the file has already been processed " + fullName);
+			File fl = new File(fullName);
+			if(fl.exists()) {
+				log.error("The file has already been processed."+f.getName());
+				removeFile(f);
+				return false;
+			}else {
+				log.info("File  is accepted for processing:"+f.getName());
+			}
+			return true;
 		}
-		return shouldFilebeAccepted;
+		return false;
+	}
+	
+	@SuppressWarnings("unused")
+	private void moveFile(File input, String newDir) {
+		File f1 = new File(newDir + input.getName());
+
+		if (input.renameTo(f1)) {
+			log.info("File moved successful :" + input.getAbsolutePath());
+		} else {
+			log.info("File failed to move :" + input.getAbsolutePath());
+		}
 		
+	}
+	
+	private void removeFile(File input) {
+		input.delete();
 	}
 	
 	public static void main(String[] args) {
 		File f = new File("/Users/abhisheksingh/ddrive/everge_ws/f/Sample Lien File-112916-NEW.txt");
-//		EvergeFileListFilter<File> obj = new EvergeFileListFilter<>();
-//		obj.accept(f);
-		
-		EvergeFileListFilter<File> filter = new LienFileFilter<>();
-		boolean x = filter.shouldFilebeAccepted(f);
-		System.out.println("x is " + x);
+		EvergeFileListFilter<File> obj = new EvergeFileListFilter<>();
+		obj.accept(f);
 	}
 
 	public Configuration getConfiguration() {
