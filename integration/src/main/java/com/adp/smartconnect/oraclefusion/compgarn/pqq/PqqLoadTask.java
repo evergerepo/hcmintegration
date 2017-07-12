65c8e4cc6e3a5047f7d92f79925bec2ec0cf04ca
evergerepo <singha@evergegroup.com>
2017-07-12 21:27:05 +0530

    Updated with Kondala's changes

--- integration/src/main/java/com/adp/smartconnect/oraclefusion/compgarn/pqq/PqqLoadTask.java
+++ integration/src/main/java/com/adp/smartconnect/oraclefusion/compgarn/pqq/PqqLoadTask.java
@@ -1,18 +1,17 @@
 package com.adp.smartconnect.oraclefusion.compgarn.pqq;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.adp.smartconnect.oraclefusion.compgarn.batchloader.impl.BatchLoadTask;
 
 import bip_webservice.proxy.AccessDeniedException;
 import bip_webservice.proxy.InvalidParametersException;
@@ -28,7 +27,7 @@ import bip_webservice.proxy.types.ReportResponse;
 
 public class PqqLoadTask {
 	
-	private static final Logger log = LoggerFactory.getLogger(BatchLoadTask.class);
+	private static final Logger log = LoggerFactory.getLogger(PqqLoadTask.class);
 	
 	private String url;
 	
@@ -44,50 +43,62 @@ public class PqqLoadTask {
 		this.url = url;
 	}
 	
-    public void callRunReport(String reportPath ,Map data,String username ,String password ,String format ,String template ,String outFile ) throws AccessDeniedException, InvalidParametersException, OperationFailedException, IOException {
+	@SuppressWarnings("rawtypes")
+	public String callRunReport(String reportPath ,Map data,String username ,String password ,String format ,String template ) throws Exception{
 
-    	java.net.URL url = new java.net.URL(this.getUrl());
-    	ReportService_Service reportServiceService = new ReportService_Service(url, new QName("http://xmlns.oracle.com/oxp/service/v2", "ReportService"));
-        ReportService reportService =  reportServiceService.getReportService();
-        ParamNameValues paramNameValues = new ParamNameValues();
-        // Calling runReport
-        ReportRequest reportRequest = new ReportRequest();
-        reportRequest.setReportAbsolutePath(reportPath);
-        reportRequest.setAttributeTemplate(template);
-        reportRequest.setAttributeFormat(format);
-        reportRequest.setAttributeLocale(this.locale);
-        reportRequest.setSizeOfDataChunkDownload(this.chunkSizeDownload);
-        
-        Set keySet = data.keySet();
-        Iterator keySetItr = keySet.iterator();
-        ArrayOfParamNameValue paramNameValue = new ArrayOfParamNameValue();
-        
-        while(keySetItr.hasNext()) {
-        	String keyData = (String)keySetItr.next();
-            ArrayOfString value = new ArrayOfString();
-            value.getItem().add((String)data.get(keyData));
-            ParamNameValue reportPrompt = new ParamNameValue();
-            reportPrompt.setName(keyData);
-            reportPrompt.setValues(value);
-            log.info("keyData "+keyData);
-            log.info("value "+(String)data.get(keyData));
-            paramNameValue.getItem().add(reportPrompt);
+		String reportResponse =null;
+        try {
+        		log.debug("callRunReport() :"+reportPath+","+data+","+username+","+password+", URL:"+url); 
+               
+                ReportService_Service reportServiceService = new ReportService_Service(new URL(url), new QName("http://xmlns.oracle.com/oxp/service/v2", "ReportService"));
+                ReportService reportService =  reportServiceService.getReportService();
+                BindingProvider bp = (BindingProvider)reportService;
+                bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, url);
+                
+                
+                ParamNameValues paramNameValues = new ParamNameValues();
+                // Calling runReport
+                ReportRequest reportRequest = new ReportRequest();
+                reportRequest.setReportAbsolutePath(reportPath);
+                reportRequest.setAttributeTemplate(template);
+                reportRequest.setAttributeFormat(format);
+                reportRequest.setAttributeLocale(this.locale);
+                reportRequest.setSizeOfDataChunkDownload(this.chunkSizeDownload);
+                
+                Set keySet = data.keySet();
+                Iterator keySetItr = keySet.iterator();
+                ArrayOfParamNameValue paramNameValue = new ArrayOfParamNameValue();
+                
+                while(keySetItr.hasNext()) {
+                	String keyData = (String)keySetItr.next();
+                    ArrayOfString value = new ArrayOfString();
+                    value.getItem().add((String)data.get(keyData));
+                    ParamNameValue reportPrompt = new ParamNameValue();
+                    reportPrompt.setName(keyData);
+                    reportPrompt.setValues(value);
+                    log.debug("keyData "+keyData+", value "+(String)data.get(keyData));
+                    paramNameValue.getItem().add(reportPrompt);
+                }
+                
+                //set Parameters
+                paramNameValues.setListOfParamNameValues(paramNameValue);
+                reportRequest.setParameterNameValues(paramNameValues);
+                ReportResponse repResponse = new ReportResponse();
+                
+                //Call Report Service
+                log.info("Invoking Report Service. Request:"+reportRequest);
+                repResponse = reportService.runReport(reportRequest, username, password);
+            
+                byte[] baReport = repResponse.getReportBytes();
+                reportResponse = new String( baReport);
+                log.info("Report service response: "+ new String( baReport));
+        } catch (Exception ex) {
+        	log.error("callRunReport() Error:"+ex.getMessage(), ex);
+        	throw ex;
+        	
         }
-        
-        //set Parameters
-        paramNameValues.setListOfParamNameValues(paramNameValue);
-
-        reportRequest.setParameterNameValues(paramNameValues);
-        ReportResponse repResponse = new ReportResponse();
-        repResponse = reportService.runReport(reportRequest, username, password);
-    
-        byte[] baReport = repResponse.getReportBytes();
-        System.out.println("Response "+ new String( baReport));
-        FileOutputStream fio = new FileOutputStream(outFile);
-        fio.write(baReport);
-        fio.close();
-
-}
+        return reportResponse;
+	}
 
 	public String getUrl() {
 		return url;
@@ -123,22 +134,24 @@ public class PqqLoadTask {
 		
 		
 		Map<String, String> data = new HashMap<String, String>();
-		data.put("SSN", "220237294");
-		//data.put("SSNFormatted", "199-99-0013");
-		data.put("First Name", "DENNIS WAYNE");
-		data.put("Last Name", "BUCHANAN JR");
-		data.put("DocId", "GS119486927");
-		data.put("TransId", "1087222");
+		data.put("SSN", "199990013");
+		data.put("SSNFormatted", "199-99-0013");
+		data.put("First Name", "Natalie");
+		data.put("Last Name", "Portman");
+		data.put("DocId", "GS118268481");
+		data.put("TransId", "123");
 
+		try{
 		PqqLoadTask task = new PqqLoadTask();
-		//task.setUrl("https://hcwh-test.bi.ap1.oraclecloud.com/xmlpserver/services/v2/ReportService?wsdl");
 		task.setUrl("https://ecbfdev4-test.fs.us8.oraclecloud.com/xmlpserver/services/v2/ReportService?wsdl");
-		task.setChunkSizeDownload(-1);
-		//task.callRunReport("/Custom/Human Capital Management/Reports/Payroll/PQQ/TEST/PQQv1.25 Report.xdo", data, "HCMUser", "Cloud2World!", "xml", null, "/Users/abhisheksingh/ddrive/everge_ws/details/BIP/Report_2.xml");
+		
+		//task.callRunReport("/Custom/Human Capital Management/Reports/Payroll/PQQ/PQQ Report.xdo", data, "HCMUser", "Cloud2World!", "xml", null, "/Users/abhisheksingh/ddrive/everge_ws/details/BIP/Report_2.xml");
 
-		//task.callRunReport("/Custom/ADP Comp Garn/PQQv1.25 Report.xdo", data, "ADP_Connect", "ah2lB}A8wj05", "xml", null, "/Users/abhisheksingh/ddrive/everge_ws/details/Report_2.xml");
-		task.callRunReport("/Custom/ADP Comp Garn/PQQv1.25 Report.xdo", data, "pushparaj.dharmaraj", "Welcome123", "xml", null, "/Users/abhisheksingh/ddrive/everge_ws/details/Report_2.xml");
-
+		task.callRunReport("/Custom/ADP Comp Garn/PQQv1.25 Report.xdo", data, "ADP_Connect", "ah2lB}A8wj05", "xml", null);
+		//task.callRunReport("/Custom/ADP Comp Garn/PQQv1.25 Report.xdo", data, "pushparaj.dharmaraj", "Welcome123", "xml", null, "/Users/abhisheksingh/ddrive/everge_ws/details/Report_2.xml");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 }
