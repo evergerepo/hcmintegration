a65c49f3d97279d4391d6c24f6422fe3411fbf95
evergerepo <singha@evergegroup.com>
2017-07-12 21:24:27 +0530

    Updated with Kondala's changes

--- integration/src/main/java/com/adp/smartconnect/oraclefusion/compgarn/pqq/PqqFileHandler.java
+++ integration/src/main/java/com/adp/smartconnect/oraclefusion/compgarn/pqq/PqqFileHandler.java
@@ -2,6 +2,7 @@ package com.adp.smartconnect.oraclefusion.compgarn.pqq;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,15 +20,13 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 
 import com.adp.smartconnect.oraclefusion.compgarn.clientConfiguration.ClientConfiguration;
 import com.adp.smartconnect.oraclefusion.compgarn.clientConfiguration.PqqFileDetails;
@@ -37,7 +36,9 @@ import com.adp.smartconnect.oraclefusion.compgarn.listeners.ClientConfigHolder;
 import com.adp.smartconnect.oraclefusion.compgarn.xmltypes.PQFile;
 import com.adp.smartconnect.oraclefusion.compgarn.xmltypes.PreQualRecord;
 
-public class PqqFileHandler implements ApplicationContextAware {
+public class PqqFileHandler {
+
+	private PqqLoadTask pqqLoadTask = new PqqLoadTask();
 
 	private static final Logger logger = LoggerFactory.getLogger(PqqFileHandler.class);
 
@@ -46,16 +47,17 @@ public class PqqFileHandler implements ApplicationContextAware {
 	private Configuration configuration;
 
 	private ClientConfigHolder clientConfigurations;
-	
-	private ApplicationContext appCtx;
-	
-	private FileMover fileMover;
 
+	
+	
+	/*
+	 * UTF-8 File generation
+	 */
 	private File getUTF8File(File input) throws IOException {
 
 		// Change file encoding to UTF-8 if not already. We should also move the
 		// file to Temp - Start
-		logger.info("The absolute path is " + input.getAbsolutePath());
+		logger.debug("Start UTF-8 File creation");
 		Path path = Paths.get(input.getAbsolutePath());
 		byte[] encoded = Files.readAllBytes(path);
 		String str = new String(encoded);
@@ -75,18 +77,25 @@ public class PqqFileHandler implements ApplicationContextAware {
 		if (null == utf8File) {
 			utf8File = input;
 		}
+		logger.info("UTF-8 File created. UTF-8 File Name: " + utf8File.getAbsolutePath());
 		return utf8File;
 	}
 
-	private PQFile getPQFile(File utf8File) throws JAXBException {
-
+	/*
+	 * File data to Object Un-marshalling 
+	 */
+	private PQFile convertFileToObject(File utf8File) throws JAXBException {
 		JAXBContext jaxbContext = JAXBContext.newInstance(PQFile.class);
 		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 		PQFile file = (PQFile) jaxbUnmarshaller.unmarshal(utf8File);
 		return file;
 	}
 
-	private void callRunReport(PreQualRecord rec, PqqFileDetails fileDetails, String transID, String reportLocation)
+	
+	/*
+	 * Invoke Report Service and save response into reportLocation file
+	 */
+	private String callRunReport(PreQualRecord rec, PqqFileDetails fileDetails, String transID)
 			throws Exception {
 
 		Map<String, String> data = new HashMap<String, String>();
@@ -100,32 +109,28 @@ public class PqqFileHandler implements ApplicationContextAware {
 		data.put("Last Name", lastName);
 		data.put("DocId", docID);
 		data.put("TransId", transID);
-		
-		PqqLoadTask pqqLoadTask = (PqqLoadTask)this.appCtx.getBean("pqqLoadTask");
 
 		pqqLoadTask.setUrl(fileDetails.getPqqUrl());
 		pqqLoadTask.setLocale(fileDetails.getPqqLocale());
 		pqqLoadTask.setChunkSizeDownload(-1);
 
-		pqqLoadTask.callRunReport(fileDetails.getPqqReportPath(), data, fileDetails.getPqqUsername(),
-				fileDetails.getPqqPassword(), fileDetails.getPqqFormat(), null, reportLocation);
-
+		return pqqLoadTask.callRunReport(fileDetails.getPqqReportPath(), data, fileDetails.getPqqUsername(),
+				fileDetails.getPqqPassword(), fileDetails.getPqqFormat(), null);
 	}
+	
+	
 
-	private PQFile mergePQFileObjects(List<String> files) throws JAXBException {
 
+	/*
+	 * Merge PQQ Report Response
+	 */
+	private PQFile mergePQQReportResponse(List<String> reportResponse) throws JAXBException {
 		JAXBContext j = JAXBContext.newInstance(PQFile.class);
 		Unmarshaller u = j.createUnmarshaller();
 		List<PreQualRecord> records = new ArrayList<>();
 		PQFile que = null;
-		
-		if(files.isEmpty()) {
-			return null;
-		}
-		
-		for (String s : files) {
-			File f = new File(s);
-			que = (PQFile) u.unmarshal(f);
+		for (String responseXml : reportResponse) {
+			que = (PQFile) u.unmarshal(new StreamSource( new StringReader( responseXml.toString() ) ) );
 			que.setTransmissionID(que.getTransId());
 			que.setTransId(null);
 
@@ -136,161 +141,185 @@ public class PqqFileHandler implements ApplicationContextAware {
 
 		}
 
-		if(null != que && null != que.getPreQualRecordList()) {
-			que.getPreQualRecordList().setQualRecords(records);
-		}
-		
+		que.getPreQualRecordList().setQualRecords(records);
 		return que;
 	}
+	
 
+	/*
+	 * Create PQQ Response file
+	 */
 	private void createPQQResponseFile(ClientConfiguration config, PQFile que) throws JAXBException, IOException {
 
-		// Create directory
-		String pqqRespDir = configuration.getFileProcessingDir() + configuration.getPqqDir()
-				+ configuration.getPqqOutboundDir();
-		logger.info("Pqq response directory is " + pqqRespDir);
-
-		String outputFile = getPQResponseFileName(config);
-
-		File file1 = new File(pqqRespDir + "/" + outputFile);
-		JAXBContext jaxbContext1 = JAXBContext.newInstance(PQFile.class);
-		Marshaller marshaller = jaxbContext1.createMarshaller();
-		// To remove standalone yes from the generated xml
-		marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
-		// marshaller.setProperty("com.sun.xml.internal.bind.xmlHeaders", "<?xml
-		// version=\"1.0\" encoding=\"UTF-8\" ?>");
-
-		// To add xsd
-		marshaller.setProperty(Marshaller.JAXB_NO_NAMESPACE_SCHEMA_LOCATION, "PQFile.xsd");
-
-		java.io.FileWriter writer = new java.io.FileWriter(file1);
-		writer.append("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
-		marshaller.marshal(que, writer);
-
+		try{
+			// Create directory
+			String pqqRespDir = configuration.getFileProcessingDir() + configuration.getPqqDir()+configuration.getPqqOutboundDir();
+			String outputFile = getPQResponseFileName(config);
+			logger.info("Pqq response File: " + outputFile);
+	
+			File file1 = new File(pqqRespDir + "/" + outputFile);
+			JAXBContext jaxbContext1 = JAXBContext.newInstance(PQFile.class);
+			Marshaller marshaller = jaxbContext1.createMarshaller();
+			// To remove standalone yes from the generated xml
+			marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
+			// marshaller.setProperty("com.sun.xml.internal.bind.xmlHeaders", "<?xml
+			// version=\"1.0\" encoding=\"UTF-8\" ?>");
+	
+			// To add xsd
+			marshaller.setProperty(Marshaller.JAXB_NO_NAMESPACE_SCHEMA_LOCATION, "PQFile.xsd");
+	
+			java.io.FileWriter writer = new java.io.FileWriter(file1);
+			writer.append("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
+			marshaller.marshal(que, writer);
+		
+			logger.info("PQQ Respons file created: File Name"+outputFile);
+		}catch(Exception e){
+			logger.error("Error while creating PQQ Respons file:"+e.getMessage(), e);
+			throw new IOException(e.getCause());
+		}
 	}
 
+	
+	
+	/*
+	 * Handle Input PQQ File
+	 */
 	public File handleFile(File input) throws IOException {
 
 		boolean isPartDone = false;
 		boolean isError = false;
-		boolean noAction = false;
 		File inProcessingFile = null;
 		File doneFile = null;
 		File partFile = null;
 		File errorFile = null;
 		PqqFileDetails fileDetails = null;
+		List<String> reportResponseList = new ArrayList<String>();
+		
 		try {
 			String fileName = input.getName();
-			MDC.put("transactionID", fileName);
-			logger.info("handleFile PqqFileHandler to handle the file " + input.getAbsolutePath());
+			MDC.put("fileName", fileName);
+			logger.info("PQQ File processing STARTED. File Name: " + input.getAbsolutePath());
 
 			clientName = fileName.substring(0, 4).toLowerCase();
-			logger.info("The client id in Pqq is " + clientName);
+			logger.info("The client id in Pqq is :" + clientName);
 			ClientConfiguration config = clientConfigurations.getSingleClientData(clientName);
 
 			// Check if the file is getting processed
-			String dirName = FilenameUtils.getBaseName(fileName);
+			String inboundFileBaseName = FilenameUtils.getBaseName(fileName);
 			fileDetails = config.getPqqFileDetails();
-			inProcessingFile = new File(configuration.getFileProcessingDir() + configuration.getPqqDir()
-					+ configuration.getPqqProcessedDir() + dirName + ".processing");
-			doneFile = new File(configuration.getFileProcessingDir() + configuration.getPqqDir()
-					+ configuration.getPqqProcessedDir() + dirName + ".done");
-			partFile = new File(configuration.getFileProcessingDir() + configuration.getPqqDir()
-					+ configuration.getPqqProcessedDir() + dirName + ".part.done");
-			errorFile = new File(configuration.getFileProcessingDir() + configuration.getPqqDir()
-					+ configuration.getPqqProcessedDir() + dirName + ".error");
+			String processingDir = configuration.getFileProcessingDir() + configuration.getPqqDir()+ configuration.getPqqProcessedDir();
 
-			if (inProcessingFile.exists() || doneFile.exists() || partFile.exists() || errorFile.exists()) {
-				noAction = true;
+			inProcessingFile = new File(processingDir + inboundFileBaseName + ".processing");
+			doneFile = new File(processingDir + inboundFileBaseName + ".done");
+			partFile = new File(processingDir + inboundFileBaseName + ".part.done");
+			errorFile = new File(processingDir + inboundFileBaseName + ".error");
+
+			
+			if (inProcessingFile.exists() || doneFile.exists()) {
+				logger.warn("Processing/Done file found, stop processing file.");;
 				return null;
 			}
+			
 			// Create in processing file
-			logger.info("inProcessingFile is " + inProcessingFile.getAbsolutePath());
+			logger.info("Create Processing File: File Name:" + inProcessingFile.getAbsolutePath());
 			inProcessingFile.createNewFile();
 
-			// Get the UTF 8 File and Place it in archive
+			// Get the UTF 8 File
 			File utf8File = getUTF8File(input);
-			PQFile file = getPQFile(utf8File);
-
+			PQFile file = convertFileToObject(utf8File);
+			
 			List<PreQualRecord> qualRecords = file.getPreQualRecordList().getQualRecords();
 			String transID = file.getTransmissionID();
-			int i = 1;
-			List<String> files = new ArrayList<>();
+			logger.info("No of PQQ records in file:"+file.getPreQualRecordList().getQualRecords().size());
+			
 
+			int i = 1;
 			Map<String, Boolean> successRecs = new HashMap<>();
 			Map<String, Boolean> failureRecs = new HashMap<>();
 
+			//Process each PQQ Record
 			for (PreQualRecord rec : qualRecords) {
-				logger.debug("Pqq processing for the ith record " + i);
-				String reportLocation = configuration.getFileProcessingDir() + configuration.getPqqDir()
-						+ configuration.getPqqProcessedDir() + "/SubReport_"+ dirName + "_" + i + ".xml";		
 				String key = null;
 				try {
 					key = rec.getSSN() + " " + rec.getEEFirstName();
-					callRunReport(rec, fileDetails, transID, reportLocation);
+					String reportResponse = callRunReport(rec, fileDetails, transID);
+					reportResponseList.add(reportResponse);
 					successRecs.put(key, true);
-					files.add(reportLocation);
 				} catch (Exception exc) {
 					failureRecs.put(key, false);
-					logger.error("Exeception while invoking the callRunReport", exc);
+					logger.error("Error processing PQQ Record. Record Key:"+key, exc);
 				}
 
 				i = i + 1;
 			}
-
-			isPartDone = (successRecs.size() != 0 && failureRecs.size() != 0);
 			
+
+			//Check for any Exceptions
+			isPartDone = (failureRecs.size() != 0);
 			if(isPartDone) {
-				// Print the summary
-				logger.error("Error happened for the following SSN and first name combinations ");
+				logger.error("No of Failed Records:"+failureRecs.size()+",  Sucessful records:"+successRecs.size());
 				Iterator<String> failRecsItr = failureRecs.keySet().iterator();
 				while(failRecsItr.hasNext()) {
 					String key = (String)failRecsItr.next();
-					logger.error(key);
+					logger.error("Error Record Key:"+key);
 				}
-				logger.error("Total "+ failureRecs.size() + " records failed the PQQ Processing.");
-				logger.error("Total "+ successRecs.size() + " records succeeded the PQQ Processing.");
 			}
+			
 
 			// Merge PQFile objects
-			PQFile que = mergePQFileObjects(files);
+			//PQFile que = mergePQFileObjects(responsePartFiles);
+			
+			PQFile pqFile = mergePQQReportResponse(reportResponseList);
 
 			// Create the response file
-			if(null != que) {
-				createPQQResponseFile(config, que);
-			}
-			
-			isError = (successRecs.size() == 0);
+			createPQQResponseFile(config, pqFile);
 
-		} catch (Exception exc) {	
-			logger.error("Exception in PqqFileHandler ", exc);
+		} catch (Exception exc) {
+			logger.error("handleFile() Exception in PqqFileHandler. Message: "+exc.getMessage(), exc);
 			errorFile.createNewFile();
-			isError = true;	
+			isError = true;
 		} finally {
-			logger.debug("noAction " + noAction);
-			logger.debug("isPartDone " + isPartDone);
-			logger.debug("isError " + isError);
-			
-			if(!noAction) {
-				
-				if (!isError && isPartDone) {
-					partFile.createNewFile();
-				} else if(!isError) {
-					doneFile.createNewFile();
-				} else if (isError) {
-					errorFile.createNewFile();
-				}
-				inProcessingFile.delete();
-
-				logger.debug("Removing the file");
-				fileMover.removeFile(input);
-				
+			if (!isError && isPartDone) {
+				partFile.createNewFile();
+			} else if(!isError) {
+				doneFile.createNewFile();
 			}
-			
+			postProcess( input,  inProcessingFile);
 		}
+		
+		
 		MDC.clear();
+		logger.info("PQQ File processing COMPLETED. File Name: " + input.getAbsolutePath());
 		return null;
 	}
+	
+	
+	/*
+	 * Post process after completing the process
+	 */
+	private void postProcess(File input, File inProcessingFile){
+		
+		try{
+			
+			//1. Copy Input file to Archive Folder
+			FileMover.handleFile(input, configuration.getFileProcessingDir()+configuration.getPqqDir()+configuration.getPqqArchiveDir());
+			
+			//2. Remove Input File
+			FileMover.removeFile(input);
+			
+			//3. Remove In-Process Temp File
+			inProcessingFile.delete();
+
+		}catch(Exception e){
+			logger.error("PQQ postProcess error. Message:"+e.getMessage(), e);
+		}
+		
+	}
+	
+	
+	
+	
+	
 
 	private String getPQResponseFileName(ClientConfiguration config) {
 		// Create Pqq Format
@@ -367,17 +396,12 @@ public class PqqFileHandler implements ApplicationContextAware {
 		this.clientConfigurations = clientConfigurations;
 	}
 
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.appCtx = applicationContext;		
+	public PqqLoadTask getPqqLoadTask() {
+		return pqqLoadTask;
 	}
 
-	public FileMover getFileMover() {
-		return fileMover;
-	}
-
-	public void setFileMover(FileMover fileMover) {
-		this.fileMover = fileMover;
+	public void setPqqLoadTask(PqqLoadTask pqqLoadTask) {
+		this.pqqLoadTask = pqqLoadTask;
 	}
 
 }
